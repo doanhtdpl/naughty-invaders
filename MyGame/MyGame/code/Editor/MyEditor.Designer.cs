baa7bdf117d5357ff6b3a1f5832e4dcb2049da69
pitiku <pitiku@eda0479d-ffe4-9923-0ece-56ae051a5b6f>
2011-01-25 22:46:20 +0000

    Editor tab

--- MyGame/MyGame/code/Editor/MyEditor.Designer.cs
+++ MyGame/MyGame/code/Editor/MyEditor.Designer.cs
@@ -29,26 +29,51 @@
         private void InitializeComponent()
         {
             this.myEditorControl1 = new MyGame.MyEditorControl();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
             // myEditorControl1
             // 
-            this.myEditorControl1.Location = new System.Drawing.Point(0, 0);
+            this.myEditorControl1.Location = new System.Drawing.Point(0, 60);
             this.myEditorControl1.Name = "myEditorControl1";
             this.myEditorControl1.Size = new System.Drawing.Size(1280, 720);
             this.myEditorControl1.TabIndex = 0;
             this.myEditorControl1.Text = "myEditorControl1";
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1280, 60);
+            this.tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1272, 34);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Scenario";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // NaughtyEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(1272, 693);
+            this.ClientSize = new System.Drawing.Size(1272, 753);
+            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.myEditorControl1);
             this.Name = "NaughtyEditor";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MyEditor";
+            this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -56,5 +81,7 @@
         #endregion
 
         private MyEditorControl myEditorControl1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
     }
 }
