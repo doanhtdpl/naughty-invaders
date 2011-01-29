a055245adfaa85cab12c557738ea2c8b52aa4fb4
pitiku <pitiku@eda0479d-ffe4-9923-0ece-56ae051a5b6f>
2011-01-29 17:43:12 +0000

    camera movement in editor

--- MyGame/MyGame/code/Editor/EditorStates/DefaultState.cs
+++ MyGame/MyGame/code/Editor/EditorStates/DefaultState.cs
@@ -3,24 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace MyGame
 {
     class DefaultState : EditorState
     {
-        public override void leftClick(Vector2 _vPos)
-        {
-            Ray ray = EditorHelper.Instance.getMouseCursorRay(_vPos);
-            selectedEntity = EditorHelper.Instance.rayVsEntities(ray, LevelManager.Instance.getStaticProps());
-
-            if (selectedEntity != null)
-            {
-                updateEntityProperties();
-            }
-        }
+        MouseState lastMouseState;
 
         public override void update()
         {
+            KeyboardState keyState = Keyboard.GetState();
+            MouseState mouseState = Mouse.GetState();
+            if (keyState.IsKeyDown(Keys.Space) && mouseState.LeftButton == ButtonState.Pressed)
+            {
+                Camera2D.position.X -= (mouseState.X - lastMouseState.X);
+                Camera2D.position.Y += (mouseState.Y - lastMouseState.Y);
+            }
+            else if (keyState.IsKeyDown(Keys.Space) && mouseState.RightButton == ButtonState.Pressed)
+            {
+                Camera2D.position.Z -= (mouseState.Y - lastMouseState.Y);
+            }
+            else if(keyState.GetPressedKeys().Length == 0)
+            {
+                if (mouseState.LeftButton == ButtonState.Pressed && lastMouseState.LeftButton == ButtonState.Released)
+                {
+                    System.Drawing.Point point = MyEditor.Instance.myEditorControl.PointToClient(new System.Drawing.Point(mouseState.X, mouseState.Y));
+                    Vector2 pos = new Vector2(point.X, point.Y);
+                    Ray ray = EditorHelper.Instance.getMouseCursorRay(pos);
+                    selectedEntity = EditorHelper.Instance.rayVsEntities(ray, LevelManager.Instance.getStaticProps());
+
+                    if (selectedEntity != null)
+                    {
+                        updateEntityProperties();
+                    }
+                }
+            }
+            lastMouseState = mouseState;
         }
 
         public override void render()
