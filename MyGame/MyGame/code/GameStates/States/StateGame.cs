581d0e43b127e12d58305612d1d1d1d69fc1bbed
marcos.torrecillas <marcos.torrecillas@gmail.com>
2011-01-29 13:08:11 +0000

    Added function addRectangle in DebugManager.cs. Updated some stuff and commented test code.

--- MyGame/MyGame/code/GameStates/States/StateGame.cs
+++ MyGame/MyGame/code/GameStates/States/StateGame.cs
@@ -43,9 +43,6 @@ namespace MyGame
             DebugManager.Instance.render();
             gui.render();
 
-            DebugManager.Instance.addLine(new Vector3(0, 0, 0), new Vector3(200, 200, 0), Color.Red);
-            DebugManager.Instance.addLine(new Vector3(0, 0, 0), new Vector3(200, -200, 0), Color.Green);
-            DebugManager.Instance.addLine(new Vector3(0, 0, 0), new Vector3(-200, 200, 0), Color.Blue);
             DebugManager.Instance.render();
         }
         
