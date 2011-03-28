35cfd1ec8af2a5c4810ea47caa62c680c4d5ec39
marcos.torrecillas@gmail.com <marcos.torrecillas@gmail.com@eda0479d-ffe4-9923-0ece-56ae051a5b6f>
2011-03-28 21:46:41 +0000

    - Added orb manager and orb class. Work in progress.

---
+++ trunk/MyGame/MyGame/code/Gameplay/Orbs/Orb.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace MyGame
+{
+    public class Orb
+    {
+        public enum tOrb { XP, Life, Wish, Pet }
+
+        public const float SIZE = 35.0f;
+        public const float FRICTION = 10.0f;
+        public static Texture texture;
+        
+	    public Vector2 position;
+        public Vector2 velocity;
+        public Color color;
+        public float life;
+        public bool toPlayer;
+
+        public Orb(tOrb orbType, Vector2 position)
+        {
+            this.position = position;
+            toPlayer = false;
+            life = 5.0f;
+
+            velocity = Calc.randomDirection() * Calc.randomScalar() * 20.0f;
+
+            switch (orbType)
+            {
+                case tOrb.XP:
+                    color = Color.Yellow;
+                    break;
+                case tOrb.Life:
+                    color = Color.Green;
+                    break;
+                case tOrb.Wish:
+                    color = Color.Red;
+                    break;
+                case tOrb.Pet:
+                    color = Color.Blue;
+                    break;
+            }
+        }
+    };
+}
