6bd78a8147ef24fddb51cb087999641b4ce8fe45
pitiku <pitiku@eda0479d-ffe4-9923-0ece-56ae051a5b6f>
2011-03-29 11:12:13 +0000

    scale temp

--- MyGame/MyGame/code/Editor/EditorStates/EditorState_ScaleState.cs
+++ MyGame/MyGame/code/Editor/EditorStates/EditorState_ScaleState.cs
@@ -34,7 +34,12 @@ namespace MyGame
                     {
                         foreach(Entity2D ent in MyEditor.Instance.getSelectedEntities())
                         {
-                            ent.scale2D += new Vector2(mouseState.X - lastMouseState.X, -(mouseState.Y - lastMouseState.Y));
+                            //Vector3 offset = ent.position - center;
+                            Vector3 scale = new Vector3((mouseState.X - lastMouseState.X) / 100.0f, -(mouseState.Y - lastMouseState.Y) / 100.0f, 0.0f);
+                            scale += Vector3.One;
+                            scale = Vector3.Transform(scale, Matrix.Identity);
+                            ent.scale *= scale;
+                            //ent.position = center + new Vector3(offset.X * scale.X, offset.Y * scale.Y, 0);
                         }
                     }
                     else if (mouseState.RightButton == ButtonState.Pressed)
