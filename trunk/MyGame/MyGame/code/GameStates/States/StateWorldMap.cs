d51174ba1cee7bfe4f5ca0039e2cd852d3afe805
marcos.torrecillas@gmail.com <marcos.torrecillas@gmail.com@eda0479d-ffe4-9923-0ece-56ae051a5b6f>
2011-04-03 09:53:55 +0000

    - Added worldmap state. WiP - Added CameraModes in camera manager. Now are follow player and node.

---
+++ trunk/MyGame/MyGame/code/GameStates/States/StateWorldMap.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace MyGame
+{
+    class StateWorldMap : GameState
+    {
+        public override void initialize()
+        {
+            type = StateManager.tGS.Game;
+            gameState = true;
+            longLoad = true;
+            DebugManager.Instance.initialize();
+            ParticleManager.Instance.loadXML();
+            EditorHelper.Instance.loadNewLevel("mapa");
+            CameraManager.Instance.setCameraMode(CameraManager.tCameraMode.FollowPlayer);
+        }
+
+        public override void loadContent()
+        {
+
+        }
+
+        public override void render()
+        {
+            EntityManager.Instance.render();
+            LevelManager.Instance.render();
+            DebugManager.Instance.render();
+            GUIManager.Instance.render();
+        }
+        
+        public override void update()
+        {
+            base.update();
+
+            LevelManager.Instance.update();
+            ProjectileManager.Instance.update();
+            ParticleManager.Instance.update();
+            OrbManager.Instance.update();
+            CameraManager.Instance.update();
+            GUIManager.Instance.update();
+
+            SB.cam.update();
+        }
+        
+        public override void dispose()
+        {
+        }
+    }
+}
