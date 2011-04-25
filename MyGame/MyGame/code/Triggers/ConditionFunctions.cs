a64781deb95bd924d67cf8c68cbf96a5978cd57e
marcos.torrecillas <marcos.torrecillas@gmail.com>
2011-04-25 16:47:23 +0000

    - Triggers updated and improved, now fully working.

---
+++ MyGame/MyGame/code/Triggers/ConditionFunctions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Reflection;
+
+namespace MyGame
+{
+    class ConditionFunctions
+    {
+        public static bool isPlayersLastLife()
+        {
+            return GamerManager.getSessionOwner().Player.lifes == 1;
+        }
+    }
+}
---
+++ MyGame/MyGame/code/Triggers/Trigger.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Reflection;
+
+namespace MyGame
+{
+    class Trigger
+    {
+        public int executionTimes = 1;
+
+        List<TriggerFunction> conditions = new List<TriggerFunction>();
+        List<TriggerFunction> executions = new List<TriggerFunction>();
+
+        public void addFunction(bool isCondition, string functionName, params object[] parameters)
+        {
+            string functionContainer = "";
+            if (isCondition)
+            {
+                functionContainer = "ConditionFunctions";
+            }
+            else
+            {
+                functionContainer = "ConsequenceFunctions";
+            }
+            Type type = Type.GetType("MyGame." + functionContainer);
+            MethodInfo method = type.GetMethod(functionName);
+
+            TriggerFunction tf = new TriggerFunction(method, parameters);
+
+            if (isCondition)
+            {
+                conditions.Add(tf);
+            }
+            else
+            {
+                executions.Add(tf);
+            }
+        }
+
+        public void initTrigger()
+        {
+
+        }
+
+        public bool isTriggered()
+        {
+            foreach (TriggerFunction condition in conditions)
+            {
+                if (!condition.execute())
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public bool execute()
+        {
+            foreach (TriggerFunction execution in executions)
+            {
+                execution.execute();
+            }
+            --executionTimes;
+            return executionTimes > 0;
+        }
+    }
+}
