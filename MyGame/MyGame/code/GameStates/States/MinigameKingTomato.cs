23cd0c4dbfa41ae5e8bf4b5bc7af0ec677828a7b
marcos.torrecillas <marcos.torrecillas@gmail.com>
2011-06-13 15:26:48 +0000

    - epileptic macedonia and king tomato tweaks

--- MyGame/MyGame/code/GameStates/States/MinigameKingTomato.cs
+++ MyGame/MyGame/code/GameStates/States/MinigameKingTomato.cs
@@ -216,6 +216,7 @@ namespace MyGame
         {
             base.update();
 
+            //onionElder.update();
             if (CinematicManager.Instance.cinematicToPlay != null) return;
 
             switch (state)
